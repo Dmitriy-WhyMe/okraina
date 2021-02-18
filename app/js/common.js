65158836804d2afd720c69501d634b92d485befa
Dmitriy <dima.ivahnoff@mail.ru>
2021-02-18 18:31:36 +0300

    start

---
+++ app/js/common.js
@@ -0,0 +1,6 @@
+(function ($) {
+	$(document).ready(function () {
+		import Scrollbar from 'smooth-scrollbar';
+		Scrollbar.init(document.querySelector('#my-scrollbar'), options);
+	});
+})(jQuery);
