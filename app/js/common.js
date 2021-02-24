71640ff28d5c508fff7722668cf10bc52bbfc867
Dmitriy <dima.ivahnoff@mail.ru>
2021-02-24 18:02:09 +0300

    responsive

--- app/js/common.js
+++ app/js/common.js
@@ -5,15 +5,25 @@
 			slidesToScroll: 1,
 			arrows: false,
 			fade: true,
+			adaptiveHeight: true,
 			asNavFor: '.slider-nav'
-		});
-		$('.slider-nav').slick({
+		  });
+		  $('.slider-nav').slick({
 			slidesToShow: 2,
 			slidesToScroll: 1,
-			arrows: false,
+			focusOnSelect: true,
+			adaptiveHeight: true,
 			asNavFor: '.slider-for',
-			focusOnSelect: true
-		});
+			prevArrow: $('.arrow_prev'),
+			nextArrow: $('.arrow_next')
+		  });
+		AOS.init();
+		var scene_1 = document.getElementById('scene_1');
+		var parallaxInstance_1 = new Parallax(scene_1);
+		var scene_2 = document.getElementById('scene_2');
+		var parallaxInstance_2 = new Parallax(scene_2);
+		var scene_3 = document.getElementById('scene_3');
+		var parallaxInstance_3 = new Parallax(scene_3);
 	});
 })(jQuery);
 
